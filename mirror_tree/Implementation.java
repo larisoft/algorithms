3d5ab2e00883a5738aa5b5f392495c4c477354b2
nkemdirim <nkemdirimarukwe@gmail.com>
2021-02-18 13:16:34 +0100

    started invert tree

---
+++ mirror_tree/Implementation.java
@@ -0,0 +1,16 @@
+public class Implementation{
+
+
+
+
+
+    public void getTree(){
+
+        Node root = newNode(1); 
+        root.left = newNode(2); 
+        root.right = newNode(3); 
+        root.left.left = newNode(4); 
+        root.left.right = newNode(5); 
+
+    }
+}
---
+++ mirror_tree/MirrorTree.java
@@ -0,0 +1,7 @@
+public class MirrorTree{
+
+    public static void main(String[] args){
+        
+        System.out.println("ran successfully");
+    }
+}
